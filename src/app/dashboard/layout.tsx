19c608ba0a30a8117b83606ecf889a5ee33bcf4d
Jassmesh Singh <jassmesh@nirmaan.education>
2025-12-11 11:14:35 +0000

    I am still able to see the Bottom navbar when i am opening the case stud

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from 'next';
 import { AppSidebar } from '@/components/app/app-sidebar';
 
@@ -9,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen w-full bg-gradient-to-b from-accent to-orange-200">
+        <div className="min-h-screen w-full bg-gradient-to-b from-accent to-orange-200 relative">
             <main className="p-4 md:p-6">
               {children}
             </main>
