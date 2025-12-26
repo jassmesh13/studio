923fa46f1e0e8fe642083ffe55f4934e0c169c7f
Jassmesh Singh <jassmesh@nirmaan.education>
2025-12-26 05:59:00 +0000

    everything fine it seems just it is not working offline can yyou fix it

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type {NextConfig} from 'next';
+import createPwa from '@ducanh2912/next-pwa';
+
+const withPwa = createPwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -32,4 +39,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPwa(nextConfig);
